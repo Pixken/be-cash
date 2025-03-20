1b5828701605bc4d3c9e2e90074b9129cde076d9
xsz <3110801700@qq.com>
2025-03-20 23:48:37 +0800

    chore: 更改应用名称

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'be-cash',
+  appName: '鸟蛋记账',
   webDir: 'dist'
 };
 
