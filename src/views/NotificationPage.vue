f7b4fe95f534bcb92317c5689bf985751c83b0bb
许斯钊 <3110801700@qq.com>
2025-07-24 13:58:30 +0800

    修复保存通知请求

--- src/views/NotificationPage.vue
+++ src/views/NotificationPage.vue
@@ -1,5 +1,6 @@
 <template>
   <ion-page>
+    <div class="aa"></div>
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>通知监听</ion-title>
@@ -12,7 +13,7 @@
           <ion-title size="large">通知监听</ion-title>
         </ion-toolbar>
       </ion-header>
-      
+      <button @click="test">test</button>
       <NotificationListener />
     </ion-content>
   </ion-page>
@@ -21,4 +22,28 @@
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import NotificationListener from '@/components/NotificationListener.vue';
+
+const test = () => {
+  fetch('http://117.72.49.27:3000/notification', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: '1',
+        content: '1',
+        appName: '1'
+      })
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.error('发送通知失败:', err);
+    }); 
+}
 </script>
+
+<style scoped>  
+.aa {
+  padding-top: env(safe-area-inset-top);
+}
+</style>
